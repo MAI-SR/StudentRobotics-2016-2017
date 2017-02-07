5cc67e2de24a6a54130d2fd18ac9d8a00f498017
MAI-SR <mai.studentrobotics@gmail.com>
2017-02-07 17:23:31 +0100

    Fixed some bugs and added methods for the steppermotors

--- CustomRug.ino
+++ CustomRug.ino
@@ -1,4 +1,3 @@
-#include <PinChangeInt.h>
 #include <Arduino.h>
 
 // We communicate with the power board at 115200 baud.
@@ -6,20 +5,32 @@
 
 #define FW_VER 0
 
-int const trigUST = A1;  //token - Analogue Pin
+const int rightStepPin = 2;
+const int rightDirPin = 3;
+const int leftStepPin = 4;
+const int leftDirPin = 5;
 
-int const echoUST = 10;
+int trig = 7;//COMP/TRIG auf US
+int pwm = 8;//PWM auf US
+
+unsigned int ticks = 0;
+
+uint8_t EnPwmCmd[4]={0x44,0x02,0xbb,0x01};    // distance measure command[copy-paste]
 
 void setup() {
   Serial.begin(SERIAL_BAUD);
-  resetBEDPins(); //Set step, direction, microstep and enable pins to default states
-  pinMode(trigUST, OUTPUT);
-  digitalWrite(trigUST, HIGH);
-  pinMode(echoUST, INPUT);
-  
-  for(int i=0;i<4;i++) {
+  pinMode(rightStepPin, OUTPUT);
+  pinMode(rightDirPin, OUTPUT);
+  pinMode(leftStepPin, OUTPUT);
+  pinMode(leftDirPin, OUTPUT);
+  pinMode(trig, OUTPUT);
+  pinMode(pwm, INPUT);
+  digitalWrite(trig, HIGH);
+  for(int i=0;i<4;i++)
+  {
       Serial.write(EnPwmCmd[i]);
   } 
+}
 
 int read_pin() {
   while (!Serial.available());
@@ -55,61 +66,179 @@ void command_mode(int mode) {
   pinMode(pin, mode);
 }
 
-void readUS(int trigPin, int echoPin){
-  digitalWrite(trigPin, LOW);                // a low pull on pin COMP/TRIG  triggering a sensor reading
-  digitalWrite(trigPin, HIGH);               // reading Pin PWM will output pulses
+void rightTurn()
+{
+  digitalWrite(rightDirPin, HIGH);
+  digitalWrite(leftDirPin, HIGH);
+  tick();
+}
+
+void leftTurn()
+{
+  digitalWrite(rightDirPin, LOW);
+  digitalWrite(leftDirPin, LOW);
+  tick();
+}
+
+void forwardDrive()
+{
+  digitalWrite(rightDirPin, HIGH);
+  digitalWrite(leftDirPin, LOW);
+  tick();
+}
+
+void backwardDrive()
+{
+  digitalWrite(rightDirPin, LOW);
+  digitalWrite(leftDirPin, HIGH);
+  tick();
+}
+
+void tick()
+{
+  int x = 0;
+  while(x < ticks)
+  {
+    digitalWrite(rightStepPin, HIGH);
+    digitalWrite(leftStepPin, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(rightStepPin, LOW);
+    digitalWrite(leftStepPin, LOW);
+    delayMicroseconds(1000);
+    x = x+1;
+  }
+  ticks = 0;
+}
+
+void readUS(){                              // a low pull on pin COMP/TRIG  triggering a sensor reading
+  digitalWrite(trig, LOW);
+  digitalWrite(trig, HIGH);               // reading Pin PWM will output pulses
     
-  unsigned long DistanceMeasured=pulseIn(echoPin,LOW);
-    
-  if(DistanceMeasured>=25000) {              // the reading is invalid.
-    Serial.println(10000);    
-  } else {
-    unsigned int DistanceReal = DistanceMeasured/50;           // every 50us low level stands for 1cm
-    Serial.print(DistanceReal);
+  unsigned long DistanceMeasured=pulseIn(pwm,LOW);
+  unsigned int Distance = 0;
+  if(DistanceMeasured>=10200)
+  {              // the reading is invalid.
+    Serial.print("300"); //used to be: Serial.println("Invalid");
+  }
+  else
+  {
+    Distance=DistanceMeasured/50;           // every 50us low level stands for 1cm
+    Serial.print(Distance);
   }  
 }
 
 void loop() {
   // Fetch all commands that are in the buffer
   while (Serial.available()) {
-    digitalWrite(EN, LOW); //Pull enable pin low to set FETs active and allow motor control
     int selected_command = Serial.read();
     // Do something different based on what we got:
     switch (selected_command) {
       case 'a':
         command_analogue_read();
+        Serial.print("\n");
         break;
       case 'r':
         command_read();
+        Serial.print("\n");
         break;
       case 'l':
         command_write(LOW);
+        Serial.print("\n");
         break;
       case 'h':
         command_write(HIGH);
+        Serial.print("\n");
         break;
       case 'i':
         command_mode(INPUT);
+        Serial.print("\n");
         break;
       case 'o':
         command_mode(OUTPUT);
+        Serial.print("\n");
         break;
       case 'p':
         command_mode(INPUT_PULLUP);
+        Serial.print("\n");
         break;
       case 'v':
         Serial.print("SRcustom:");
         Serial.print(FW_VER);
+        Serial.print("\n");
         break;
       case 'b':
-        readUS(trigUST, echoUST);
+        readUS();
+        Serial.print("\n");
+        break;
+      case 'c':
+        Serial.print("\n");
+        forwardDrive();
+        break;
+      case 'd':
+        Serial.print("\n");
+        backwardDrive();
+        break;
+      case 'e':
+        Serial.print("\n");
+        rightTurn();
+        break;
+      case 'f':
+        Serial.print("\n");
+        leftTurn();
+        break;
+      case 'g':
+        Serial.print("\n");
+        ticks = ticks+5000;
+        break;
+      case 'j':
+        Serial.print("\n");
+        ticks = ticks+2000;
+        break;
+      case 'k':
+        Serial.print("\n");
+        ticks = ticks+1000;
+        break;
+      case 'm':
+        Serial.print("\n");
+        ticks = ticks+500;
+        break;
+      case 'n':
+        Serial.print("\n");
+        ticks = ticks+200;
+        break;
+      case 'q':
+        Serial.print("\n");
+        ticks = ticks+100;
+        break;
+      case 's':
+        Serial.print("\n");
+        ticks = ticks+50;
+        break;
+      case 't':
+        Serial.print("\n");
+        ticks = ticks+20;
+        break;
+      case 'u':
+        Serial.print("\n");
+        ticks = ticks+10;
+        break;
+      case 'w':
+        Serial.print("\n");
+        ticks = ticks+5;
+        break;
+      case 'x':
+        Serial.print("\n");
+        ticks = ticks+2;
+        break;
+      case 'y':
+        Serial.print("\n");
+        ticks = ticks+1;
         break;
       default:
+        Serial.print("\n");
         // A problem here: we do not know how to handle the command!
         // Just ignore this for now.
         break;
     }
-    Serial.print("\n");
   }
-}
 }
